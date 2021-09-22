b1ef273bb74a26ba8ac4043a2c1830767fe536bb
athul <athulkrishnama24@gmail.com>
2021-09-22 19:30:36 +0530

    added action to all numeric buttons

--- src/Cal.java
+++ src/Cal.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,7 +12,9 @@ import javax.swing.SwingConstants;
 
 
 
-public class Cal {
+
+
+public class Cal implements ActionListener{
 	
 	//frame of calculator
 	
@@ -50,15 +54,18 @@ public class Cal {
 		//numeric buttons
 		
 		sevenbut=new JButton("7");
+		sevenbut.addActionListener(this);
 		sevenbut.setBounds(5,65,80,80);
 		calFrame.add(sevenbut);	  
 				
 		eightbut=new JButton("8");
 		eightbut.setBounds(90,65,80,80);
+		eightbut.addActionListener(this);
 		calFrame.add(eightbut);
 				
 		ninebut=new JButton("9");
 		ninebut.setBounds(175,65,80,80);
+		ninebut.addActionListener(this);
 		calFrame.add(ninebut);
 		
 
@@ -69,14 +76,17 @@ public class Cal {
 		
 		
 		fourbut=new JButton("4");
+		fourbut.addActionListener(this);
 		fourbut.setBounds(5,150,80,80);
 		calFrame.add(fourbut);	  
 				
 		fivebut=new JButton("5");
+		fivebut.addActionListener(this);
 		fivebut.setBounds(90,150,80,80);
 		calFrame.add(fivebut);
 				
 		sixbut=new JButton("6");
+		sixbut.addActionListener(this);
 		sixbut.setBounds(175,150,80,80);
 		calFrame.add(sixbut);
 		
@@ -88,14 +98,17 @@ public class Cal {
 		
 		
 		onebut=new JButton("1");
+		onebut.addActionListener(this);
 		onebut.setBounds(5,235,80,80);
 		calFrame.add(onebut);	  
 				
 		twobut=new JButton("2");
+		twobut.addActionListener(this);
 		twobut.setBounds(90,235,80,80);
 		calFrame.add(twobut);
 				
 		threebut=new JButton("3");
+		threebut.addActionListener(this);
 		threebut.setBounds(175,235,80,80);
 		calFrame.add(threebut);
 		
@@ -107,14 +120,17 @@ public class Cal {
 		
 		
 		dotbut=new JButton(".");
+		dotbut.addActionListener(this);
 		dotbut.setBounds(5,320,80,80);
 		calFrame.add(dotbut);	  
 				
 		zerobut=new JButton("0");
+		zerobut.addActionListener(this);
 		zerobut.setBounds(90,320,80,80);
 		calFrame.add(zerobut);
 				
 		equalbut=new JButton("=");
+		equalbut.addActionListener(this);
 		equalbut.setBounds(175,320,80,80);
 		calFrame.add(equalbut);
 
@@ -122,31 +138,78 @@ public class Cal {
 		
 		
 		
-		//arithematic buttons
+		//arithmetic buttons
 		
 		plusbut=new JButton("+");
+		plusbut.addActionListener(this);
 		plusbut.setBounds(260,65,80,80);
 		calFrame.add(plusbut);	 
 		
 		minusbut=new JButton("-");
+		minusbut.addActionListener(this);
 		minusbut.setBounds(260,150,80,80);
 		calFrame.add(minusbut);	 
 		
 		mulbut=new JButton("X");
+		mulbut.addActionListener(this);
 		mulbut.setBounds(260,235,80,80);
 		calFrame.add(mulbut);	 
 		
 		divbut=new JButton("/");
+		divbut.addActionListener(this);
 		divbut.setBounds(260,320,80,80);
 		calFrame.add(divbut);
 		
 		clearbut=new JButton("CE");
+		clearbut.addActionListener(this);
 		clearbut.setBounds(345, 65, 80, 165);
 		calFrame.add(clearbut);
 		
 	}
 	public static void main(String[] args) {
 		new Cal();
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==sevenbut) {
+			display.setText(display.getText()+"7");
+		}else if(e.getSource()==eightbut) {
+			display.setText(display.getText()+"8");
+			
+		}else if (e.getSource()==ninebut) {
+			display.setText(display.getText()+"9");
+			
+		}else if (e.getSource()==sixbut) {
+			display.setText(display.getText()+"6");
+			
+		}else if (e.getSource()==fivebut) {
+			display.setText(display.getText()+"5");
+			
+		}else if (e.getSource()==fourbut) {
+			display.setText(display.getText()+"4");
+			
+		}else if (e.getSource()==threebut) {
+			display.setText(display.getText()+"3");
+			
+		}else if (e.getSource()==twobut) {
+			display.setText(display.getText()+"2");
+			
+		}else if (e.getSource()==onebut) {
+			display.setText(display.getText()+"1");
+			
+		}else if (e.getSource()==zerobut) {
+			display.setText(display.getText()+"0");
+			
+		}else if (e.getSource()==dotbut) {
+			display.setText(display.getText()+".");
+			
+		}else if(e.getSource()==clearbut) {
+			display.setText("");
+		}
+		
+		
+		
 		
 	}
 
